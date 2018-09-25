c5842384b2440bec18d587a57e58e10b3a9b1560
PRANEETH THOTA <praneeth.thota@pnmac.com>
2018-09-25 14:09:03 +0000

    added associations and books

--- user_schema.js
+++ user_schema.js
@@ -10,8 +10,31 @@ const {
 const graphSequel = require('graphql-sequelize');
 var db = require('./models/index')
 const { User } =  db.User
+const _ = require('lodash')
+// dummy data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: 1, userId: 2 },
+  { name: 'The Final Empire', genre: 'Fantasy', id: 2, userId: 2 },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: 4, userId: 2 },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: 3, userId: 3 },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: 5, userId: 3 },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: 6, userId: 4 },
+];
 
-
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: ( ) => ({
+      id: { type: GraphQLInt },
+      name: { type: GraphQLString },
+      genre: { type: GraphQLString },
+      user: {
+          type: UserType,
+          resolve(parent, args){
+            return db.User.findOne({where: {id: parent.userId}})
+          }
+      }
+  })
+});
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
@@ -19,7 +42,13 @@ const UserType = new GraphQLObjectType({
     firstName: {type: GraphQLString},
     lastName: {type: GraphQLString},
     email: {type: GraphQLString},
-    username: {type: GraphQLString}
+    username: {type: GraphQLString},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, { userId: parent.id });
+      }
+}
   })
 })
 const RootQuery = new GraphQLObjectType({
@@ -29,7 +58,6 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: {id: {type: GraphQLInt}},
       resolve(parent, args){
-        //return _.find(users, {id: args.id})
         return db.User.findOne({where: {id: args.id}})
       }
 
@@ -39,6 +67,21 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(UserType),
       resolve(parent, args){
           return db.User.findAll();
+      }
+    },
+    // fetch book
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args){
+          return _.find(books, { id: args.id });
+      }
+    },
+    // fetch all books
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+          return books;
       }
     }
   }
